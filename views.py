d89b006a72e0aeef39b97e5b22b12f4372d3da13
Vas Vasiliadis <vas@exordium.com>
2018-05-07 15:56:47 -0500

    Flask initial commit

---
+++ views.py
@@ -0,0 +1,180 @@
+# views.py
+#
+# Copyright (C) 2011-2018 Vas Vasiliadis
+# University of Chicago
+#
+# Application logic for the GAS
+#
+##
+__author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
+
+import uuid
+import time
+from datetime import datetime
+
+import boto3
+from botocore.client import Config
+from boto3.dynamodb.conditions import Key
+
+from flask import (abort, flash, redirect, render_template, 
+  request, session, url_for)
+
+from gas import app, db
+from decorators import authenticated, is_premium
+from auth import get_profile, update_profile
+
+
+"""Start annotation request
+Create the required AWS S3 policy document and render a form for
+uploading an annotation input file using the policy document
+"""
+@app.route('/annotate', methods=['GET'])
+@authenticated
+def annotate():
+  # Open a connection to the S3 service
+  s3 = boto3.client('s3', 
+    region_name=app.config['AWS_REGION_NAME'], 
+    config=Config(signature_version='s3v4'))
+
+  bucket_name = app.config['AWS_S3_INPUTS_BUCKET']
+
+  # Generate unique ID to be used as S3 key (name)
+  key_name = app.config['AWS_S3_KEY_PREFIX'] + str(uuid.uuid4()) + '~${filename}'
+
+  # Redirect to a route that will call the annotator
+  redirect_url = str(request.url) + "/job"
+
+  # Define policy conditions
+  # NOTE: We also must inlcude "x-amz-security-token" since we're
+  # using temporary credentials via instance roles
+  encryption = app.config['AWS_S3_ENCRYPTION']
+  acl = app.config['AWS_S3_ACL']
+  expires_in = app.config['AWS_SIGNED_REQUEST_EXPIRATION']
+  fields = {
+    "success_action_redirect": redirect_url,
+    "x-amz-server-side-encryption": encryption,
+    "acl": acl
+  }
+  conditions = [
+    ["starts-with", "$success_action_redirect", redirect_url],
+    {"x-amz-server-side-encryption": encryption},
+    {"acl": acl}
+  ]
+
+  # Generate the presigned POST call
+  presigned_post = s3.generate_presigned_post(Bucket=bucket_name, 
+    Key=key_name, Fields=fields, Conditions=conditions, ExpiresIn=expires_in)
+
+  # Render the upload form which will parse/submit the presigned POST
+  return render_template('annotate.html', s3_post=presigned_post)
+
+
+"""Fires off an annotation job
+Accepts the S3 redirect GET request, parses it to extract 
+required info, saves a job item to the database, and then
+publishes a notification for the annotator service.
+"""
+@app.route('/annotate/job', methods=['GET'])
+@authenticated
+def create_annotation_job_request():
+  # Parse redirect URL query parameters for S3 object info
+  bucket_name = request.args.get('bucket')
+  key_name = request.args.get('key')
+  job_id = str(uuid.uuid4())
+
+  # Persist job to database
+  # Move your code here...
+
+  # Send message to request queue
+  # Move your code here...
+
+  return render_template('annotate_confirm.html', job_id=job_id)
+
+
+"""List all annotations for the user
+"""
+@app.route('/annotations', methods=['GET'])
+@authenticated
+def annotations_list():
+  pass
+
+
+"""Display details of a specific annotation job
+"""
+@app.route('/annotations/<id>', methods=['GET'])
+@authenticated
+def annotation_details(id):
+  pass
+
+
+"""Display the log file for an annotation job
+"""
+@app.route('/annotations/<id>/log', methods=['GET'])
+@authenticated
+def annotation_log(id):
+  pass
+
+
+"""Subscription management handler
+"""
+import stripe
+
+@app.route('/subscribe', methods=['GET', 'POST'])
+@authenticated
+def subscribe():
+  pass
+
+
+"""DO NOT CHANGE CODE BELOW THIS LINE
+*******************************************************************************
+"""
+
+"""Home page
+"""
+@app.route('/', methods=['GET'])
+def home():
+  return render_template('home.html')
+
+"""Login page; send user to Globus Auth
+"""
+@app.route('/login', methods=['GET'])
+def login():
+  app.logger.info('Login attempted from IP {0}'.format(request.remote_addr))
+  # If user requested a specific page, save it to session for redirect after authentication
+  if (request.args.get('next')):
+    session['next'] = request.args.get('next')
+  return redirect(url_for('authcallback'))
+
+"""404 error handler
+"""
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('error.html', 
+    title='Page not found', alert_level='warning',
+    message="The page you tried to reach does not exist. Please check the URL and try again."), 404
+
+"""403 error handler
+"""
+@app.errorhandler(403)
+def forbidden(e):
+  return render_template('error.html',
+    title='Not authorized', alert_level='danger',
+    message="You are not authorized to access this page. If you think you deserve to be granted access, please contact the supreme leader of the mutating genome revolutionary party."), 403
+
+"""405 error handler
+"""
+@app.errorhandler(405)
+def not_allowed(e):
+  return render_template('error.html',
+    title='Not allowed', alert_level='warning',
+    message="You attempted an operation that's not allowed; get your act together, hacker!"), 405
+
+"""500 error handler
+"""
+@app.errorhandler(500)
+def internal_error(error):
+  return render_template('error.html',
+    title='Server error', alert_level='danger',
+    message="The server encountered an error and could not process your request."), 500
+
+### EOF
