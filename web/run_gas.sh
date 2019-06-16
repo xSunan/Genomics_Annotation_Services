616b71d5c7b582feb2c7a2970b5605de42c94157
Vas Vasiliadis <vas@exordium.com>
2019-06-16 02:01:56 -0500

    Reorganized utilities

--- web/run_gas.sh
+++ web/run_gas.sh
@@ -2,23 +2,23 @@
 
 # run_gas.sh
 #
-# Copyright (C) 2011-2018 Vas Vasiliadis
+# Copyright (C) 2011-2019 Vas Vasiliadis
 # University of Chicago
 #
 # Rns the GAS app using the Gunicorn server for production environments
 #
 ##
 
-cd /home/ubuntu/gas
-source /home/ubuntu/gas/.env
-[[ -d /home/ubuntu/gas/log ]] || mkdir /home/ubuntu/gas/log
-if [ ! -e /home/ubuntu/gas/log/$GAS_LOG_FILE_NAME ]; then
-    touch /home/ubuntu/gas/log/$GAS_LOG_FILE_NAME;
+cd /home/ubuntu/gas/web
+source /home/ubuntu/gas/web/.env
+[[ -d /home/ubuntu/gas/web/log ]] || mkdir /home/ubuntu/gas/web/log
+if [ ! -e /home/ubuntu/gas/web/log/$GAS_LOG_FILE_NAME ]; then
+    touch /home/ubuntu/gas/web/log/$GAS_LOG_FILE_NAME;
 fi
 if [ "$1" = "console" ]; then
     LOG_TARGET=-
 else
-    LOG_TARGET=/home/ubuntu/gas/log/$GAS_LOG_FILE_NAME
+    LOG_TARGET=/home/ubuntu/gas/web/log/$GAS_LOG_FILE_NAME
 fi
 /home/ubuntu/.virtualenvs/mpcs/bin/gunicorn --log-file=$LOG_TARGET \
 --log-level=debug --workers=$GUNICORN_WORKERS --certfile=/usr/local/src/ssl/ucmpcs.org.crt \
