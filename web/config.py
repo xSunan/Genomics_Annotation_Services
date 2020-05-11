e70d134f4638d5f35eddfd8792252e40f1e22599
Vas Vasiliadis <vas@exordium.com>
2020-05-11 12:39:11 -0500

    Updates for Spring 2020

--- web/config.py
+++ web/config.py
@@ -17,28 +17,35 @@ from botocore.exceptions import ClientError
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):  
-  GAS_LOG_LEVEL = os.environ['GAS_LOG_LEVEL'] if ('GAS_LOG_LEVEL' in os.environ) else 'INFO'
-  GAS_LOG_FILE_PATH = basedir + (os.environ['GAS_LOG_FILE_PATH'] if ('GAS_LOG_FILE_PATH' in os.environ) else "/log")
-  GAS_LOG_FILE_NAME = os.environ['GAS_LOG_FILE_NAME'] if ('GAS_LOG_FILE_NAME' in os.environ) else "gas.log"
+  GAS_LOG_LEVEL = os.environ['GAS_LOG_LEVEL'] \
+    if ('GAS_LOG_LEVEL' in os.environ) else 'INFO'
+  GAS_LOG_FILE_PATH = basedir + (os.environ['GAS_LOG_FILE_PATH'] \
+    if ('GAS_LOG_FILE_PATH' in os.environ) else "/log")
+  GAS_LOG_FILE_NAME = os.environ['GAS_LOG_FILE_NAME'] \
+    if ('GAS_LOG_FILE_NAME' in os.environ) else "gas.log"
 
   WSGI_SERVER = 'werkzeug'
   
   CSRF_ENABLED = True
   SECRET_KEY = os.environ['SECRET_KEY']
-  SSL_CERT_PATH = os.environ['SSL_CERT_PATH'] if ('SSL_CERT_PATH' in os.environ) else "../ssl/server_dev.crt"
-  SSL_KEY_PATH = os.environ['SSL_KEY_PATH'] if ('SSL_KEY_PATH' in os.environ) else "../ssl/server_dev.key"
+  SSL_CERT_PATH = os.environ['SSL_CERT_PATH'] \
+    if ('SSL_CERT_PATH' in os.environ) else "../ssl/server_dev.crt"
+  SSL_KEY_PATH = os.environ['SSL_KEY_PATH'] \
+    if ('SSL_KEY_PATH' in os.environ) else "../ssl/server_dev.key"
 
   GAS_HOST_IP = os.environ['GAS_HOST_IP']
   GAS_HOST_PORT = int(os.environ['GAS_HOST_PORT'])
   GAS_APP_HOST = os.environ['GAS_APP_HOST']
-  GAS_SERVER_NAME = os.environ['GAS_HOST_IP'] + ":" + os.environ['GAS_HOST_PORT']
+  GAS_SERVER_NAME = f"{os.environ['GAS_HOST_IP']}:{os.environ['GAS_HOST_PORT']}"
   
   GAS_CLIENT_ID = os.environ['GAS_CLIENT_ID']
   GAS_CLIENT_SECRET = os.environ['GAS_CLIENT_SECRET']
-  GLOBUS_AUTH_LOGOUT_URI = 'https://auth.globus.org/v2/web/logout'
+  GLOBUS_AUTH_LOGOUT_URI = "https://auth.globus.org/v2/web/logout"
 
-  AWS_PROFILE_NAME = os.environ['AWS_PROFILE_NAME'] if ('AWS_PROFILE_NAME' in  os.environ) else None
-  AWS_REGION_NAME = os.environ['AWS_REGION_NAME'] if ('AWS_REGION_NAME' in  os.environ) else "us-east-1"
+  AWS_PROFILE_NAME = os.environ['AWS_PROFILE_NAME'] \
+    if ('AWS_PROFILE_NAME' in  os.environ) else None
+  AWS_REGION_NAME = os.environ['AWS_REGION_NAME'] \
+    if ('AWS_REGION_NAME' in  os.environ) else "us-east-1"
 
   # Get RDS secret from AWS Secrets Manager and construct database URI
   asm = boto3.client('secretsmanager', region_name=AWS_REGION_NAME)
@@ -46,15 +53,30 @@ class Config(object):
     asm_response = asm.get_secret_value(SecretId='rds/accounts_database')
     rds_secret = json.loads(asm_response['SecretString'])
   except ClientError as e:
-    print(f"Unable to retrieve RDS credentials from AWS Secrets Manager: {e}")
+    print(f"Unable to retrieve accounts database credentials from ASM: {e}")
     raise e
 
   SQLALCHEMY_DATABASE_TABLE = os.environ['ACCOUNTS_DATABASE_TABLE']
-  SQLALCHEMY_DATABASE_URI = "postgresql://" + rds_secret['username'] + ':' + rds_secret['password'] + \
-    '@' + rds_secret['host'] + ':' + str(rds_secret['port']) + '/' + SQLALCHEMY_DATABASE_TABLE
+  SQLALCHEMY_DATABASE_URI = "postgresql://" + \
+    rds_secret['username'] + ':' + rds_secret['password'] + \
+    '@' + rds_secret['host'] + ':' + str(rds_secret['port']) + \
+    '/' + SQLALCHEMY_DATABASE_TABLE
   SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-  AWS_SIGNED_REQUEST_EXPIRATION = 300  # validity of pre-signed POST requests (in seconds)
+  # Get the Stripe API credentials from ASM
+  try:
+    asm_response = asm.get_secret_value(SecretId='stripe/mpcs_dev_account')
+    stripe_keys = json.loads(asm_response['SecretString'])
+  except ClientError as e:
+    print(f"Unable to retrieve Stripe API credentials from ASM: {e}")
+    raise e
+
+  # Set Stripe API keys
+  STRIPE_PUBLIC_KEY = stripe_keys['api_public_key']
+  STRIPE_SECRET_KEY = stripe_keys['api_secret_key']
+
+  # Set validity of pre-signed POST requests (in seconds)
+  AWS_SIGNED_REQUEST_EXPIRATION = 60
 
   AWS_S3_INPUTS_BUCKET = "gas-inputs"
   AWS_S3_RESULTS_BUCKET = "gas-results"
@@ -73,14 +95,11 @@ class Config(object):
   # Change the table name to your own
   AWS_DYNAMODB_ANNOTATIONS_TABLE = "<CNetID>_annotations"
 
-  # Stripe API keys
-  STRIPE_PUBLIC_KEY = "<your_stripe_TEST_pulic_key>"
-  STRIPE_SECRET_KEY = "<your_stripe_TEST_secret_key>"
-
   # Change the email address to your username
   MAIL_DEFAULT_SENDER = "<CNetID>@ucmpcs.org"
 
-  FREE_USER_DATA_RETENTION = 600 # time before free user results are archived (in seconds)
+  # Time before free user results are archived (in seconds)
+  FREE_USER_DATA_RETENTION = 600
 
 class DevelopmentConfig(Config):
   DEBUG = True
@@ -90,8 +109,10 @@ class ProductionConfig(Config):
   DEBUG = False
   GAS_LOG_LEVEL = 'INFO'
   WSGI_SERVER = 'gunicorn.error'
-  SSL_CERT_PATH = os.environ['SSL_CERT_PATH'] if ('SSL_CERT_PATH' in os.environ) else "/usr/local/src/ssl/ucmpcs.org.crt"
-  SSL_KEY_PATH = os.environ['SSL_KEY_PATH'] if ('SSL_KEY_PATH' in os.environ) else "/usr/local/src/ssl/ucmpcs.org.key"
+  SSL_CERT_PATH = os.environ['SSL_CERT_PATH'] \
+    if ('SSL_CERT_PATH' in os.environ) else "/usr/local/src/ssl/ucmpcs.org.crt"
+  SSL_KEY_PATH = os.environ['SSL_KEY_PATH'] \
+    if ('SSL_KEY_PATH' in os.environ) else "/usr/local/src/ssl/ucmpcs.org.key"
 
 class StagingConfig(Config):
   STAGING = True
